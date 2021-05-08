1c67e59c2b42016b438d48a92e5cee87925a4df7
yahaod <yahaod@student.unimelb.edu.au>
2021-05-08 22:22:19 +1000

    install docker

--- MRC/install_dependency.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-
-
-. ./unimelb-comp90024-2021-grp-15-openrc.sh; ansible-playbook --ask-become-pass install_dependency.yaml
---
+++ MRC/set_docker.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+. ./unimelb-comp90024-2021-grp-15-openrc.sh; ansible-playbook -i hosts.ini -u ubuntu --key-file=~/.ssh/ansible.pem set_docker.yaml
+
+#chmod +x ./set_docker.sh
