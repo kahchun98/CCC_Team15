7c34023f9f420700101f8189cbb087d4a43fb3ab
yahaod <yahaod@student.unimelb.edu.au>
2021-05-10 14:46:31 +1000

    Docker containers with CouchDB

--- ansible/run-nectar.sh
+++ ansible/run-nectar.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-. ./unimelb-comp90024-2021-grp-15-openrc.sh; ansible-playbook --ask-become-pass mrc.yaml
+. ./unimelb-comp90024-2021-grp-15-openrc.sh; ansible-playbook -i hosts.ini -u ubuntu --key-file=~/.ssh/ansible.pem --ask-become-pass mrc.yaml
