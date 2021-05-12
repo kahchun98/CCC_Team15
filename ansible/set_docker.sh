24b8f59fe89880cbe81c11ba69f4f1f30574d85b
yahaod <yahaod@student.unimelb.edu.au>
2021-05-12 11:32:52 +1000

    Update

--- ansible/set_docker.sh
+++ ansible/set_docker.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-. ./unimelb-comp90024-2021-grp-15-openrc.sh; ansible-playbook -i hosts.ini -u ubuntu --key-file=~/.ssh/ansible.pem set_docker.yaml
+. ./unimelb-comp90024-2021-grp-15-openrc.sh; ansible-playbook -i inventory/hosts.ini -u ubuntu --key-file=~/.ssh/ansible.pem set_docker.yaml
 
 #chmod +x ./set_docker.sh
