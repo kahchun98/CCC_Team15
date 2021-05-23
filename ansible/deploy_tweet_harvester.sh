082c1808731a58e73634764148034d46e4684fb6
yahaod <yahaod@student.unimelb.edu.au>
2021-05-23 13:59:59 +1000

    File name

---
+++ ansible/deploy_tweet_harvester.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+. ./unimelb-comp90024-2021-grp-15-openrc.sh; ansible-playbook -vvvv -i inventory/hosts.ini -u ubuntu --key-file=~/.ssh/ansible.pem deploy_tweet_harvester.yaml
+
+#chmod +x ./set_docker.sh
